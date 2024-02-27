8fef572321acf0ac7eabded7ffbced719f527268
DarshanMistari <mistaridarshan2@gmail.com>
2024-02-27 21:34:54 +0530

    [ADDED]:Darshan - Java code for String Concatention

---
+++ StringConcat.java
@@ -0,0 +1,10 @@
+class StringConcat
+{
+	public static void main(String arg[])
+	{
+		String name="Java is Object Oriented ";
+		String name2 ="Programming Language";
+
+		System.out.println(name.concat(name2));
+	}
+}
