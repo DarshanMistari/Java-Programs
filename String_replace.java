240db63ae9f145c38ee28896b1b3e6fa818d95d1
DarshanMistari <mistaridarshan2@gmail.com>
2024-02-29 17:53:10 +0530

    [ADDED]:Darshan - Java code for String Replace

---
+++ String_replace.java
@@ -0,0 +1,13 @@
+class String_replace
+{
+	public static void main(String args[])
+	{
+
+	String lang = "Java Programming";
+
+	String replaces = lang.replace("Java Programming","Java is Objectb Oriented Programmig Language");
+
+	System.out.println("String Replace :" + replaces);
+
+    }
+}
