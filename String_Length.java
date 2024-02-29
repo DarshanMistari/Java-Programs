9b3c892487da68892f9db61ab6b608fb9cc7f1e5
DarshanMistari <mistaridarshan2@gmail.com>
2024-02-29 17:55:43 +0530

    [ADDED]:Darshan - Java code for Count String Length

---
+++ String_Length.java
@@ -0,0 +1,11 @@
+class String_Length
+{
+	public static void main(String args[])
+	{
+		String lang = "Java is Object Oriented Laguage is developed by James Gosling.";
+
+		int length = lang.length();
+
+		System.out.println("String of Length :" + length);
+	}
+}
