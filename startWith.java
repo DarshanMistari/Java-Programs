dda6a4d505e630269a2596274e5decbcf105052f
DarshanMistari <mistaridarshan2@gmail.com>
2024-02-29 17:59:49 +0530

    [ADDED]:Darshan - Java code for String Start With Function

---
+++ startWith.java
@@ -0,0 +1,9 @@
+class startWith
+{
+	public static void main(String args[])
+	{
+		String lang = "Object Oriented Programming language";
+
+		System.out.println("String Start With :" + lang.startsWith("Obj"));
+	}
+}
