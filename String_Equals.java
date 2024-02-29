a32113a7bf30225595d4077d33e50517f1b07790
DarshanMistari <mistaridarshan2@gmail.com>
2024-02-29 17:57:41 +0530

    [ADDED]:Darshan - Java code for String is Equal or Not

---
+++ String_Equals.java
@@ -0,0 +1,12 @@
+class String_Equals
+{
+	public static void main(String args[])
+	{
+		String lang = "Java Programming Language";
+		String lang2 = "Object Oriented Programming Language";
+
+		boolean equal = lang.equals(lang2);
+
+		System.out.println("String is Equal or Not :" + equal);
+	}
+}
