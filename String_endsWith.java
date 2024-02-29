7df39939873f3424044dca5eea9ac137da25e851
DarshanMistari <mistaridarshan2@gmail.com>
2024-02-29 18:01:51 +0530

    [ADDED]:Darshan - Java code for String endsWith Function

---
+++ String_endsWith.java
@@ -0,0 +1,9 @@
+class String_endsWith
+{
+	public static void main(String args[])
+	{
+		String lang = "Object Oriented Programming Language.";
+
+		System.out.println("String EndsWith :" + lang.endsWith("age."));
+	}
+}
