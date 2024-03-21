0e967b9969ae6f1daebcaeb16f2fa5f008cb2dad
DarshanMistari <mistaridarshan2@gmail.com>
2024-03-21 18:33:26 +0530

    [ADDED]:Darshan - Commandline Arguments

---
+++ Commandline.java
@@ -0,0 +1,17 @@
+class Commandline
+{
+	public static void main(String args[])
+	{
+		int num;
+		num=Integer.parseInt(args[0]);
+
+		if(num % 2 == 0)
+		{
+			System.out.println("It is Even Number.");
+		}
+		else
+		{
+			System.out.println("It is Odd Number");
+		}
+	}
+}
