f08ce19ec4b721c2f46428443e44b49082f7392a
DarshanMistari <mistaridarshan2@gmail.com>
2024-03-28 19:39:10 +0530

    [ADDED]:Darshan - Wraper class

---
+++ WraperClass.java
@@ -0,0 +1,13 @@
+class WraperClass
+{
+	public static void main(String args[])
+	{
+		String Name="Darshan Mistari";
+		String s = new String(Name);
+		String name = Name;
+		System.out.println("Orignal String :" +Name);
+		System.out.println("Object s Wraper String :" +s);
+		System.out.println("Object name Wraper String :" +name);
+
+	}
+}
