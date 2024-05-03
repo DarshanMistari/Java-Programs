6122c2964bfecf0562a7b83e696576aaf642a8ed
DarshanMistari <mistaridarshan2@gmail.com>
2024-05-03 14:54:56 +0530

    [ADDED]: Local Variable in java

---
+++ VariableLocal.java
@@ -0,0 +1,12 @@
+class VariableLocal
+{
+	public static void main(String args[])
+	{
+		int sum;	//Local Variable are Declare inside Function
+		int a=10;	//Local Variable
+		int b=20;	//Local Variable
+
+		sum =a + b;
+		System.out.println("Addition :"+sum);
+	}
+}
