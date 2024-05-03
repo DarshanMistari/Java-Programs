7e3ca3241bc58d9e5764d8bc4f6080245c769e08
DarshanMistari <mistaridarshan2@gmail.com>
2024-05-03 14:55:58 +0530

    [ADDED]: Instance Variable in java

---
+++ VariableInstance.java
@@ -0,0 +1,14 @@
+class VariableInstance
+{
+	int a=10;	//Instance Variable
+
+	public static void main(String args[])
+	{
+
+		VariableInstance obj = new VariableInstance();	//Instance Variable always Created Object
+
+
+		System.out.println("Instance Variable :"+  obj.a  );	//Instance Variable always Created Object
+
+	}
+}
