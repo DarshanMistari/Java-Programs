71455744808475cbea361691fc04995b40292d0e
DarshanMistari <mistaridarshan2@gmail.com>
2024-08-05 22:23:29 +0530

    [ADDED]:Darshan Java code Palindromic pattern

---
+++ Patterns/Palindromec_Pattern.java
@@ -0,0 +1,26 @@
+class Palindromic_Pattern{
+
+    public static void main(String[] args) {
+        
+        int num = 5;
+
+        for(int i=1; i<=num; i++)
+        {
+            for(int j=1; j<=num-i; j++)
+            {
+                System.err.print(" ");
+            }
+
+            for(int j=i; j>=1; j--)
+            {
+                System.out.print(j);
+            }
+
+            for(int j=2; j<=i; j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
