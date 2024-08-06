7cc9b43528a276eea951281ec6dd99fbdd6b9ca1
DarshanMistari <mistaridarshan2@gmail.com>
2024-08-06 09:09:57 +0530

    [ADDED]:Darshan Java code Diamond Pattern

---
+++ Patterns/Diamond_Pattern.java
@@ -0,0 +1,35 @@
+class Diamond_Pattern{
+
+    public static void main(String[] args) {
+        
+        int num = 4;
+
+        for(int i=1; i<=num; i++)
+        {
+            for(int j=1; j<=num-i; j++)
+            {
+                System.out.print(" ");
+            }
+
+            for(int j=1; j<=2*i-1; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+        for(int i=num; i>=1; i--)
+        {
+            for(int j=1; j<=num-i; j++)
+            {
+                System.out.print(" ");
+            }
+
+            for(int j=1; j<=2*i-1; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
