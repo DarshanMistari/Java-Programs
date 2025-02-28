464bedc263bae7d7c1a95a871d3a2476b4265237
DarshanMistari <mistaridarshan2@gmail.com>
2025-02-28 19:21:17 +0530

    [ADDED]:Darshan - java Diamond plus pattern

---
+++ Diamond_Pattern_Plus.java
@@ -0,0 +1,44 @@
+
+
+public class Diamond_Pattern_Plus {
+    
+    public static void main(String[] args) {
+        int num = 5;
+
+        for(int i= 1; i<=num; i++)
+        {
+            for(int j=i; j<=num; j++)
+            {
+                System.out.print(" ");
+            }
+
+            for(int j=1; j<=i; j++)
+           {
+            System.out.print("+");
+           }
+           for(int j=1; j<=i; j++)
+           {
+            System.out.print("+");
+           }
+           System.out.println();
+        }
+
+        for(int i= 1; i<=num; i++)
+        {
+            for(int j=1; j<=i; j++)
+            {
+                System.out.print(" ");
+            }
+
+            for(int j=i; j<=num; j++)
+           {
+            System.out.print("+");
+           }
+           for(int j=i; j<=num; j++)
+           {
+            System.out.print("+");
+           }
+           System.out.println();
+        }
+    }
+}
