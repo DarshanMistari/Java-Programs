41c73ea60735f388a188c10d2fef5cad3e1a8ab5
DarshanMistari <mistaridarshan2@gmail.com>
2026-01-01 00:30:59 +0530

    [ADDED]:Darshan - For loop Program

---
+++ For_Loop.java
@@ -0,0 +1,12 @@
+package com.darshan.javapracticeprogram;
+
+public class For_Loop {
+	
+	public static void main(String args[]) {
+		
+		for(int i=1; i<=5;i++) {
+			System.out.println(i);
+		}
+	}
+
+}
