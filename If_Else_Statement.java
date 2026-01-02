0f4d3b67b5bc95ba842e0f071f970c4be092a3d6
DarshanMistari <mistaridarshan2@gmail.com>
2026-01-03 00:02:27 +0530

    [ADDED]:Darshan - if else Statement

---
+++ If_Else_Statement.java
@@ -0,0 +1,22 @@
+package com.darshan.javapracticeprogram;
+
+import java.util.Scanner;
+
+public class If_Else_Statement {
+
+	public static void main(String ares[]) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int age =0;
+		System.out.println("Enter the Age :");
+		age= sc.nextInt();
+		
+		if(age >= 18) {
+			System.out.println("You are Allow for Driving License");	
+		}
+		else {
+			System.out.println("You are Allow for Driving License");
+		}
+	}
+}
