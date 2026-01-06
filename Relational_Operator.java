02e61ab3c3b5f22ae989dd73df5e0d2fcb9c6655
DarshanMistari <mistaridarshan2@gmail.com>
2026-01-06 23:42:55 +0530

    [ADDED]:Darshan - Relational operator

---
+++ Relational_Operator.java
@@ -0,0 +1,21 @@
+package com.darshan.javapracticeprogram;
+
+public class Relational_Operator {
+	
+	public static void  main(String args[]) {
+		
+	int numOne = 20;
+	int numTwo = 10;
+	
+	System.out.println(numOne > numTwo);
+	
+	System.out.println(numOne >= numTwo);
+	
+	System.out.println(numOne < numTwo);
+	
+	System.out.println(numOne <= numTwo);
+	
+	
+	}
+
+}
