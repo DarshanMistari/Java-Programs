9788c96ee6a6317d329a9dd0b8352d10fcc56c1e
DarshanMistari <mistaridarshan2@gmail.com>
2026-01-10 17:17:39 +0530

    [ADDED]:Darshan - Unary Operator Practice

---
+++ Unary_Operator_Practice.java
@@ -0,0 +1,21 @@
+package com.darshan.javapracticeprogram;
+
+public class Unary_Operator_Practice {
+	
+	public static void main(String ares[]) {
+		
+		int a = 33;
+		int b = 20;
+		
+		int result = ++a + --b; // ++a:34 + --b:19 =	53	
+		System.out.println(result);
+		
+		
+//		result = a-- - --b; 
+//		System.out.println(result);
+		
+//		result = ++a * a--; 
+//		System.out.println(result);		
+	}
+
+}
