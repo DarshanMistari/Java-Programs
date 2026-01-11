3bd76f76176632f45a71f24a8cfc324e09220cbe
DarshanMistari <mistaridarshan2@gmail.com>
2026-01-11 09:41:33 +0530

    [ADDED]:Darshan - studentmodel abstraction

---
+++ StudentModel.java
@@ -0,0 +1,48 @@
+package com.darshan.abstraction;
+
+public class StudentModel {
+
+	private int studId;
+	private String studName;
+	private String studFees;
+	private String studbirth_date;
+	private String studMobileNumber;
+	
+	
+	public void setStudId(int studId) {
+		this.studId = studId;
+	}
+	public void setStudName(String studName) {
+		this.studName = studName;
+	}
+	public void setStudFees(String studFees) {
+		this.studFees = studFees;
+	}
+	public void setStudbirth_date(String studbirth_date) {
+		this.studbirth_date = studbirth_date;
+	}
+	public void setStudMobileNumber(String studMobileNumber) {
+		this.studMobileNumber = studMobileNumber;
+	}
+	
+	
+	public int getStudId() {
+		return studId;
+	}
+	public String getStudName() {
+		return studName;
+	}
+	public String getStudFees() {
+		return studFees;
+	}
+	public String getStudbirth_date() {
+		return studbirth_date;
+	}
+	public String getStudMobileNumber() {
+		return studMobileNumber;
+	}
+	
+	
+	
+	
+}
