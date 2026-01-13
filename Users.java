dfde7daf2363a8e4fc9ac21c942e584bb563894d
DarshanMistari <mistaridarshan2@gmail.com>
2026-01-13 21:42:04 +0530

    [ADDED]:Darshan - Filehandling user

---
+++ Users.java
@@ -0,0 +1,43 @@
+package com.darshan.filehandling;
+
+public class Users {
+	
+	private int id; 
+	private String userName;
+	private String password; 
+	
+	
+	
+	
+	public Users(int id, String userName, String password) {
+		super();
+		this.id = id;
+		this.userName = userName;
+		this.password = password;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+}
