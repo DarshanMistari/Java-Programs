e1dfca100bb1afce85d29f05b1e205cb26238ed2
DarshanMistari <mistaridarshan2@gmail.com>
2026-01-14 21:32:29 +0530

    [ADDED]:Darshan - sort String accending order

---
+++ Sort_Array_Accending_Order.java
@@ -0,0 +1,13 @@
+
+import java.util.Arrays;
+
+public class Sort_Array_Accending_Order {
+    
+    public static void main(String[] args) {
+        
+        int [] num = {21, 54, 1, 6, 4};
+
+        Arrays.sort(num);
+        System.err.println("Sorted Array in Ascending Order :" +Arrays.toString(num));
+    }
+}
