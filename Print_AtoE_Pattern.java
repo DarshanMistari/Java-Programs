ae0d1f0aabf382397b5bc2a5b134428a7bc84ab8
DarshanMistari <mistaridarshan2@gmail.com>
2026-01-19 22:25:06 +0530

    [ADDED]:Darshan - Print A to E Pattern

---
+++ Print_AtoE_Pattern.java
@@ -0,0 +1,17 @@
+package com.darshan.javapracticeprogram;
+
+public class Print_AtoE_Pattern {
+
+	public static void main(String[] args) {
+
+		for (int i = 1; i <= 5; i++) {
+			for (char j = 'A'; j <= 'E'; j++) {
+
+				System.out.print(" "+j);
+			}
+			System.out.println(" ");
+		}
+
+	}
+
+}
