20eb7455a2b9b1e2511cc46b9340864d5ccbbca3
DarshanMistari <mistaridarshan2@gmail.com>
2026-01-25 22:00:53 +0530

    [ADDED]:Darshan - InvalideAmount Exception

---
+++ InvalideAmountException.java
@@ -0,0 +1,10 @@
+package com.darshan.javapracticeprogram;
+
+public class InvalideAmountException extends RuntimeException {
+
+	public InvalideAmountException(String msg) {
+		super(msg);
+	}
+	
+
+}
