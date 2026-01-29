db7e7b0acc76e642fa23f5a2fc68620006e79368
DarshanMistari <mistaridarshan2@gmail.com>
2026-01-29 21:20:17 +0530

    [ADDED]:Darshan - File Handling Delete File

---
+++ FileHandling_fileDelete.java
@@ -0,0 +1,11 @@
+package com.darshan.javapracticeprogram;
+
+import java.io.File;
+
+public class FileHandling_fileDelete {
+
+	public static void main(String[] args) {
+		
+		File file = new File("D:\\Java\\File Handling\\Employee");
+	}
+}
